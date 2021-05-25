e4dacd97e7de2654c3dc8e17eec7966d8b346915
YUJIN KIM <yujin201800@naver.com>
2021-05-25 14:22:53 +0900

    Update model.py

--- model.py
+++ model.py
@@ -7,11 +7,10 @@ import re
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class NSMC_classifier(nn.Module):
-    def __init__(self, num_vocab, embedding_dim, hidden_dim, num_layers, out_node, drop_percent=0.2, model="RNN"):  # RNN LSTM GRU를 모두 쓰기 위해서 넣은 option이다. RNN을 default로 넣음
+    def __init__(self, num_vocab, embedding_dim, hidden_dim, num_layers, out_node, drop_percent=0.2, model="RNN"):  
         super(NSMC_classifier, self).__init__()
         self.model = model
         self.num_vocab = num_vocab
-        # embedding layer: embedding_dim 맞추기
         self.embed = nn.Embedding(num_vocab, embedding_dim)
         self.rnn = nn.RNN(embedding_dim, hidden_dim, num_layers, batch_first=True)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers, batch_first=True)
@@ -25,7 +24,7 @@ class NSMC_classifier(nn.Module):
     def forward(self, x):
         x = self.embed(x)
 
-    # 원하는 모델 골라쓰기!
+    # choose a model 
         if self.model == "RNN":
             out, hidden = self.rnn(x)
         elif self.model == "LSTM":
@@ -35,7 +34,7 @@ class NSMC_classifier(nn.Module):
             out, hidden = self.gru(x)
         else:
             print("choose a model in ['RNN', 'LSTM', 'GRU']")
-            raise # 에러 발생시킴
+            raise # raise error
 
         out = self.dropout(out)
         out = self.fc(out[:,-1,:]) 
